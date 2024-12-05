54747e783ef96d5190a26e1cba1c110b129a6057
SalllesAndr <salllesandrr@gmail.com>
2024-12-05 13:39:39 -0300

    Changing script the page/index.js file.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 function Home() {
-    return<h1>Ian baixa o volume da TV!!!</h1>
-    
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial, sans-serif" }}>
+            <h1>Meu Primeiro Projeto em Node.js do Curso.Dev</h1>
+            <p>Bem-vindo ao meu primeiro projeto desenvolvido durante o curso da Curso.Dev!</p>
+            <p>
+                Confira mais sobre mim no meu 
+                <a href="https://www.linkedin.com/in/anderson-p-sales-/" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   style={{ color: "#0072b1", textDecoration: "none", marginLeft: "5px" }}>
+                    LinkedIn
+                </a>
+                ou veja outros projetos no meu 
+                <a href="https://github.com/SalllesAnd" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   style={{ color: "#333", textDecoration: "none", marginLeft: "5px" }}>
+                    GitHub
+                </a>.
+            </p>
+            <p>🚀 Desenvolvido com Node.js e Next.js, e hospedado na Vercel.</p>
+        </div>
+    );
 }
 
 export default Home;
